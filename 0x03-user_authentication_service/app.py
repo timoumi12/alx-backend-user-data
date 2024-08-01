31214644365013b5e83312ef4338c0058acc8126
iheb <ihebtimoumi12@gmail.com>
2024-08-01 22:12:08 +0100

    login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -34,5 +34,23 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login() -> str:
+    """login"""
+    # Get user credentials from form data
+    email, password = request.form.get("email"), request.form.get("password")
+    # Check if the user's credentials are valid
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    # Create a new session for the user
+    session_id = AUTH.create_session(email)
+    # Construct a response with a JSON payload
+    response = jsonify({"email": email, "message": "logged in"})
+    # Set a cookie with the session ID on the response
+    response.set_cookie("session_id", session_id)
+    # Return the response
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
