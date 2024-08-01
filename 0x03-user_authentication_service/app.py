b869a51593250a9b7b6867cd203dbab2ee7dc4c3
iheb <ihebtimoumi12@gmail.com>
2024-08-01 21:47:03 +0100

    \users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -15,6 +15,23 @@ def index() -> str:
     """returns json payload"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def users() -> str:
+    """registers a user"""
+    # Get the email and password from form data
+    email, password = request.form.get("email"), request.form.get("password")
+    try:
+        # Register the user
+        AUTH.register_user(email, password)
+        # Respond with the following JSON payload:
+        # {"email": "<registered email>", "message": "user created"}
+        return jsonify({"email": email, "message": "user created"})
+    # If the user is already registered, catch the exception and return a
+    # JSON payload of the form: {"message": "email already registered"}
+    # and return a 400 status code
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
