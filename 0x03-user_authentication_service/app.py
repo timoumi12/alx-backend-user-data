60a981838723b9ef206ad91c98945f71a577a5e1
iheb <ihebtimoumi12@gmail.com>
2024-08-01 21:40:34 +0100

    flask app

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""flassk app"""
+import logging
+from flask import Flask, abort, jsonify, redirect, request
+from auth import Auth
+
+logging.disable(logging.WARNING)
+
+
+AUTH = Auth()
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """returns json payload"""
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
